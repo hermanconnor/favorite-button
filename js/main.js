12b569c76c0dd976eb301150d3691a44ef3ff55c
hermanconnor <hermanconnor11@gmail.com>
2022-04-21 15:58:48 -0400

    complete render functions

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const initApp = async () => {
   // get json data
   const contacts = await fetchData();
   // render data to page
+  renderContacts(contacts);
   // add listeners
 };
 
@@ -12,4 +13,51 @@ const fetchData = async () => {
   const data = await response.json();
 
   return data.results;
+};
+
+const renderContacts = (contacts) => {
+  const main = document.querySelector('main');
+  const cardsArray = [];
+
+  contacts.forEach((contact) => {
+    const elementObj = createCardElements();
+    const card = createPersonCard(elementObj, contact);
+    cardsArray.push(card);
+  });
+
+  cardsArray.forEach((card) => {
+    main.appendChild(card);
+  });
+};
+
+const createCardElements = () => {
+  const article = document.createElement('article');
+  const img = document.createElement('img');
+  const details = document.createElement('div');
+  const like = document.createElement('div');
+  const name = document.createElement('h2');
+  const email = document.createElement('p');
+  const phone = document.createElement('p');
+
+  return { article, img, details, like, name, email, phone };
+};
+
+const createPersonCard = (elemObj, person) => {
+  const { article, img, details, like, name, email, phone } = elemObj;
+
+  details.className = 'details';
+  like.classList.add('like', 'unfavorite');
+  name.textContent = `${person.name.first} ${person.name.last}`;
+  img.src = person.picture.large;
+  email.textContent = person.email;
+  phone.textContent = person.cell;
+
+  article.appendChild(img);
+  details.appendChild(name);
+  details.appendChild(email);
+  details.appendChild(phone);
+  article.appendChild(details);
+  article.appendChild(like);
+
+  return article;
 };
